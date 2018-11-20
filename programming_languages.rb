afea9d6c29b46db12a830720c52c425b24361f30
Lucy Suddenly <suddenlylucy@gmail.com>
2018-11-20 19:39:09 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,6 +1,8 @@
+require 'pry'
 def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, language|
+<<<<<<< HEAD
     language.each do |name, typeinfo|
       typeinfo.each do |type, int_or_comp|
         if new_hash[name].nil?
@@ -13,4 +15,11 @@ def reformat_languages(languages)
     end 
   end
   new_hash
+=======
+    language.each do |type, info|
+      new_hash[language] = {type, style}
+      binding.pry
+    end 
+  end 
+>>>>>>> 78e31e0c2e9e414341796bc3dcefa3a49fe6e17a
 end
